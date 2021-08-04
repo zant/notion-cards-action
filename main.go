2b014e2c3b4314f205e9d2e70f3aecf25889f8b2
gonza <gonzalo.christobal@gmail.com>
2021-08-04 13:20:15 -0400

    Init

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+  "context"
+  "log"
+  "os"
+  "strings"
+
+  "github.com/dstotijn/go-notion"
+  "github.com/joho/godotenv"
+)
+
+func getIdFromUrl(page string) string {
+  return strings.Split(page, "Card-")[1]
+}
+
+type Page struct {
+  Name string
+}
+
+type CardStatus string
+
+const cardLinked = "https://www.notion.so/Card-ef2e02bf5f0f4a37a6c7fe48ff5de280"
+
+const (
+  CardStatusCodeReview CardStatus = "Code Review"
+  CardStatusQATesting  CardStatus = "QA Testing"
+  CardStatusReleased   CardStatus = "Released"
+)
+
+func main() {
+  godotenv.Load()
+  client := notion.NewClient(os.Getenv("NOTION_KEY"))
+
+  pageId := getIdFromUrl(cardLinked)
+  databasePageProperties := &notion.DatabasePageProperties{"Status": notion.DatabasePageProperty{Select: &notion.SelectOptions{Name: string(CardStatusCodeReview)}}}
+  params := notion.UpdatePageParams{DatabasePageProperties: databasePageProperties}
+  page, err := client.UpdatePageProps(context.Background(), pageId, params)
+
+  // Create Page
+  // databasePageProperties := notion.DatabasePageProperties{"title": notion.DatabasePageProperty{Title: []notion.RichText{{Text: &notion.Text{Content: "New card"}}}}}
+  // params := notion.CreatePageParams{ParentID: databaseId, ParentType: notion.ParentTypeDatabase, DatabasePageProperties: &databasePageProperties}
+  // page, err := client.CreatePage(context.Background(), params)
+
+  if err != nil {
+    log.Fatalf("Error %s", err)
+  }
+
+  properties := page.Properties.(notion.DatabasePageProperties)
+  status := properties["Status"].Select.Name
+  title := properties["Name"].Title[0].Text.Content
+
+  log.Println("\""+title+"\"", "successfully updated to:", status)
+}
