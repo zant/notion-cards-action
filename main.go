c323e61da05b888593068bc32da72fb7b55c2e6c
gonza <gonzalo.christobal@gmail.com>
2021-08-04 13:45:55 -0400

    Update to handle events

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
   "context"
+  "encoding/json"
   "log"
+  "net/http"
   "os"
   "strings"
 
   "github.com/dstotijn/go-notion"
+  "github.com/go-playground/webhooks/v6/github"
   "github.com/joho/godotenv"
 )
 
@@ -28,23 +31,39 @@ const (
   CardStatusReleased   CardStatus = "Released"
 )
 
+func getJson(url string, target interface{}) error {
+  httpClient := &http.Client{}
+  r, err := httpClient.Get(url)
+  if err != nil {
+    return err
+  }
+  defer r.Body.Close()
+  return json.NewDecoder(r.Body).Decode(target)
+}
+
+func handleError(err error) {
+  log.Fatalf("Error %s", err)
+}
+
 func main() {
   godotenv.Load()
   client := notion.NewClient(os.Getenv("NOTION_KEY"))
+
+  path := os.Getenv("GITHUB_EVENT_PATH")
+  payload := github.PullRequestPayload{}
+  err := getJson(path, payload)
+  handleError(err)
 
   pageId := getIdFromUrl(cardLinked)
   databasePageProperties := &notion.DatabasePageProperties{"Status": notion.DatabasePageProperty{Select: &notion.SelectOptions{Name: string(CardStatusCodeReview)}}}
   params := notion.UpdatePageParams{DatabasePageProperties: databasePageProperties}
   page, err := client.UpdatePageProps(context.Background(), pageId, params)
+  handleError(err)
 
   // Create Page
   // databasePageProperties := notion.DatabasePageProperties{"title": notion.DatabasePageProperty{Title: []notion.RichText{{Text: &notion.Text{Content: "New card"}}}}}
   // params := notion.CreatePageParams{ParentID: databaseId, ParentType: notion.ParentTypeDatabase, DatabasePageProperties: &databasePageProperties}
   // page, err := client.CreatePage(context.Background(), params)
-
-  if err != nil {
-    log.Fatalf("Error %s", err)
-  }
 
   properties := page.Properties.(notion.DatabasePageProperties)
   status := properties["Status"].Select.Name
